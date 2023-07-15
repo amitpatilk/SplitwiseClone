674a45947ae2b7fdbe3e9f2462fd8da48b6eb7ee
amitpatilk <79409807+amitpatilk@users.noreply.github.com>
2023-07-14 23:06:15 -0700

    first commit

---
+++ Controller/CalculatorViewController.swift
@@ -0,0 +1,70 @@
+//
+//  ViewController.swift
+//  Tipsy
+//
+//  Created by Angela Yu on 09/09/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class CalculatorViewController: UIViewController {
+    
+    
+    @IBOutlet weak var billTextField: UITextField!
+    @IBOutlet weak var zeroPctButton: UIButton!
+    @IBOutlet weak var tenPctButton: UIButton!
+    @IBOutlet weak var twentyPctButton: UIButton!
+    @IBOutlet weak var splitNumberLabel: UILabel!
+    
+    var tip = 0.10
+    var numberOfPeople = 2
+    var billTotal = 0.0
+    
+    @IBAction func tipChanged(_ sender: UIButton) {
+        
+        billTextField.endEditing(true)
+        zeroPctButton.isSelected = false
+        tenPctButton.isSelected = false
+        twentyPctButton.isSelected = false
+        
+        sender.isSelected = true
+        
+        let buttonTittle = sender.currentTitle!
+        
+        let buttonTtittleMinusSign = String(buttonTittle.dropLast())
+        
+        let buttonTittleAsNumber = Double(buttonTtittleMinusSign)!
+        
+        tip = buttonTittleAsNumber / 100
+    }
+    
+    @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        splitNumberLabel.text = String(format: "%.0f", sender.value)
+                
+                //Set the numberOfPeople property as the value of the stepper as a whole number.
+        numberOfPeople = Int(sender.value)
+        
+    }
+    
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+//        print(tip)
+        let bill = billTextField.text!
+        
+        if bill != ""{
+            billTotal = Double(bill)!
+            
+            let result = billTotal * (1 + tip) / Double(numberOfPeople)
+            
+            let rersultTo2DecimalPlaces = String(format: "%.2f", result)
+        }
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
